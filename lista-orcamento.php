3d67035c3cc06c09182da69c9f7bf28e18db9350
jaum1234 <joaovitorsouzacoura@gmail.com>
2022-01-11 06:43:43 -0300

    Implementando método que retorna lista de orcamentos finalizados

--- lista-orcamento.php
+++ lista-orcamento.php
@@ -1,6 +1,7 @@
 <?php
 
 use DesignPattern\Orcamento;
+use DesignPattern\ListaDeOrcamentos;
 
 require_once 'vendor/autoload.php';
 
@@ -22,13 +23,18 @@ $orcamento3->aprovar();
 $orcamento3->finalizar();
 $orcamento3->valor = 530.40;
 
-$listaOrcamentos = [
-    $orcamento1,
-    $orcamento2,
-    $orcamento3
-];
+//$listaOrcamentos = [
+//    $orcamento1,
+//    $orcamento2,
+//    $orcamento3
+//];
 
-foreach ($listaOrcamentos as $orcamento) {
+$listaOrcamentos = new ListaDeOrcamentos();
+$listaOrcamentos->addOrcamento($orcamento1);
+$listaOrcamentos->addOrcamento($orcamento2);
+$listaOrcamentos->addOrcamento($orcamento3);
+
+foreach ($listaOrcamentos->orcamentosFinalizados() as $orcamento) {
     echo PHP_EOL;
 
     echo "Valor: " . $orcamento->valor . PHP_EOL;
@@ -37,3 +43,13 @@ foreach ($listaOrcamentos as $orcamento) {
 
     echo PHP_EOL;
 }
+
+//foreach ($listaOrcamentos as $orcamento) {
+//    echo PHP_EOL;
+//
+//    echo "Valor: " . $orcamento->valor . PHP_EOL;
+//    echo "Estado: " . get_class($orcamento->estadoAtual) . PHP_EOL;
+//    echo "Qtd. itens: " . $orcamento->qtdItens . PHP_EOL;
+//
+//    echo PHP_EOL;
+//}
--- src/ListaDeOrcamentos.php
+++ src/ListaDeOrcamentos.php
@@ -1,10 +1,15 @@
-<?php 
+<?php
+namespace DesignPattern;
 
+use Traversable;
+use ArrayIterator;
+use IteratorAggregate;
 use DesignPattern\Orcamento;
+use DesignPattern\EstadosOrcamento\Finalizado;
 
 class ListaDeOrcamentos implements IteratorAggregate //\Iterator
 {
-    private Orcamento $orcamentos;
+    private array $orcamentos;
 
     public function __construct()
     {
@@ -13,13 +18,20 @@ class ListaDeOrcamentos implements IteratorAggregate //\Iterator
 
     public function addOrcamento(Orcamento $orcamento)
     {
-        $this->orcamentos = $orcamento;
+        $this->orcamentos[] = $orcamento;
     }
 
     //public function orcamentos()
     //{
     //    return $this->orcamentos;
     //}
+
+    public function orcamentosFinalizados()
+    {
+        return array_filter($this->orcamentos, function ($orcamento) {
+            return $orcamento->estadoAtual == new Finalizado();
+        });
+    }
 
     public function getIterator(): Traversable
     {
